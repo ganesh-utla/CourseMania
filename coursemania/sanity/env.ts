73a50dbbab55080ffa036b440ccf8016e8ef63e8
Ganesh Utla <ganeshutla888@gmail.com>
2023-10-30 19:40:22 +0530

    update sanity resources

--- coursemania/sanity/env.ts
+++ coursemania/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-30'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-20'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -8,6 +8,11 @@ export const dataset = assertValue(
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+export const token = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_TOKEN,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
